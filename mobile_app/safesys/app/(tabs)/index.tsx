49cf2f6fbc14c2d9af76c8c797c784c2c9687db4
Sahibjot Singh <75125850+sahibjotsingh05@users.noreply.github.com>
2024-11-09 20:28:00 -0700

    Initial Mobile App Commit
    App meets minimum requirements of project

---
+++ mobile_app/safesys/app/(tabs)/index.tsx
@@ -0,0 +1,205 @@
+import { StyleSheet, Dimensions, Button } from "react-native";
+import { Text, View } from "@/components/Themed";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
+const { width, height } = Dimensions.get("window");
+
+export default function TabOneScreen() {
+  const [placeholderText, setPlaceholderText] = useState("Loading...");
+  const [suspiciousActivity, setSuspiciousActivity] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [lockerId, setLockerId] = useState(null);
+
+  useEffect(() => {
+    registerForPushNotifications();
+
+    const checkAndValidateToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("userToken");
+        if (!token) {
+          setPlaceholderText("Login Required");
+          return;
+        }
+
+        const response = await axios.post(
+          "https://geekyblinders.pythonanywhere.com/validate_jwt",
+          { token }
+        );
+
+        const { status, jwt, locker, user } = response.data;
+
+        if (status && jwt === "Valid") {
+          if (locker) {
+            // Call the API to check for suspicious activity
+            setUserId(user);
+            setLockerId(locker);
+            checkSuspiciousActivity(user);
+          } else {
+            setPlaceholderText("No Lockers");
+          }
+        } else {
+          await AsyncStorage.removeItem("userToken");
+          setPlaceholderText("Login Required");
+        }
+      } catch (error) {
+        console.error("JWT Validation Error:", error);
+        setPlaceholderText("Login Required");
+      }
+    };
+
+    const checkSuspiciousActivity = async (userId: number) => {
+      try {
+        const formData = {
+          user: userId,
+        };
+        const response = await axios.post(
+          "https://geekyblinders.pythonanywhere.com/latest_activity",
+          formData
+        );
+
+        if (response.data.status) {
+          setPlaceholderText("Suspicious Activity Detected");
+          setSuspiciousActivity(true);
+        } else {
+          setPlaceholderText("Locker Secured");
+          setSuspiciousActivity(false);
+        }
+      } catch (error) {
+        console.error("Suspicious Activity Check Error:", error);
+        setPlaceholderText("Error checking activity");
+      }
+    };
+
+    checkAndValidateToken();
+
+    const interval = setInterval(() => {
+      if (userId) {
+        checkSuspiciousActivity(userId);
+      }
+    }, 5000);
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus === "granted") {
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        await AsyncStorage.setItem("expoPushToken", token);
+        console.log("Push notification token:", token);
+      } else {
+        alert("Failed to get push token for notifications!");
+      }
+    } else {
+      alert("Must use a physical device for Push Notifications");
+    }
+  };
+
+  const handleIgnore = async () => {
+    try {
+      if (userId && lockerId) {
+        await axios.post("https://geekyblinders.pythonanywhere.com/ignore", {
+          user: userId,
+          locker: lockerId,
+        });
+        setSuspiciousActivity(false);
+        setPlaceholderText("Ignored");
+      } else {
+        setPlaceholderText("Error: Missing User or Locker ID");
+      }
+    } catch (error) {
+      console.error("Error ignoring activity:", error);
+      setPlaceholderText("Error ignoring activity");
+    }
+  };
+
+  const handleReport = async () => {
+    try {
+      if (userId && lockerId) {
+        await axios.post("https://geekyblinders.pythonanywhere.com/report", {
+          user: userId,
+          locker: lockerId,
+        });
+        setSuspiciousActivity(false);
+        setPlaceholderText("Reported");
+      } else {
+        setPlaceholderText("Error: Missing User or Locker ID");
+      }
+    } catch (error) {
+      console.error("Error reporting activity:", error);
+      setPlaceholderText("Error reporting activity");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.rectangle}>
+        <Text style={styles.rectangleText}>{placeholderText}</Text>
+      </View>
+      {suspiciousActivity && (
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonWrapper}>
+            <Button title="Ignore" onPress={handleIgnore} />
+          </View>
+          <View style={styles.buttonWrapper}>
+            <Button title="Report" onPress={handleReport} />
+          </View>
+        </View>
+      )}
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  rectangle: {
+    width: width * 0.9,
+    height: height * 0.25,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  rectangleText: {
+    fontSize: 23,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    justifyContent: "space-around",
+    width: width * 0.9,
+  },
+  buttonWrapper: {
+    width: "49%",
+  },
+});
---
+++ mobile_app/safesys/app/(tabs)/two.tsx
@@ -0,0 +1,268 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const Two = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    checkUserToken();
+  }, []);
+
+  const checkUserToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem("userToken");
+      if (token) {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.error("Error checking user token", error);
+    }
+  };
+
+  const toggleForm = () => setIsLogin(!isLogin);
+
+  const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const handleLogin = async () => {
+    console.log("Logging in...");
+
+    if (!email || !password) {
+      Alert.alert("Error", "Email and password are required.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      Alert.alert("Error", "Invalid email format.");
+      return;
+    }
+
+    const loginData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://geekyblinders.pythonanywhere.com/login",
+        loginData
+      );
+
+      console.log(response.data);
+
+      if (response.data.success) {
+        await AsyncStorage.setItem("userToken", response.data.token);
+        Alert.alert("Success", "Login successful!");
+        setIsLoggedIn(true);
+      } else {
+        Alert.alert("Error", response.data.message);
+      }
+    } catch (error) {
+      console.error("Login error", error);
+      Alert.alert("Error", "Login Failed - Wrong Credentials");
+    }
+  };
+
+  const handleSignup = async () => {
+    console.log("Signing up...");
+
+    if (!name || !email || !password || !confirmPassword) {
+      Alert.alert("Error", "All fields are required.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      Alert.alert("Error", "Invalid email format.");
+      return;
+    }
+    if (password !== confirmPassword) {
+      Alert.alert("Error", "Passwords do not match.");
+      return;
+    }
+
+    const notificationToken = await getTokenFromStorage();
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+    console.log("Notification Token:", notificationToken);
+
+    const signupData = {
+      name: name,
+      email: email,
+      password: password,
+      notificationToken: notificationToken,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://geekyblinders.pythonanywhere.com/signup",
+        signupData
+      );
+
+      console.log(response.data);
+
+      if (response.data.success) {
+        await AsyncStorage.setItem("userToken", response.data.token);
+        Alert.alert("Success", "Signup successful!");
+        setIsLoggedIn(true);
+      } else {
+        Alert.alert("Error", response.data.message);
+      }
+    } catch (error) {
+      console.error("Signup error", error);
+      Alert.alert(
+        "Error",
+        "An error occurred during signup. Please try again."
+      );
+    }
+  };
+
+  const getTokenFromStorage = async () => {
+    try {
+      const token = await AsyncStorage.getItem("expoPushToken");
+      return token;
+    } catch (error) {
+      console.error("Token retrieval error", error);
+      return null;
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem("userToken");
+      setIsLoggedIn(false);
+      Alert.alert("Success", "Logged out successfully!");
+    } catch (error) {
+      console.error("Logout error", error);
+      Alert.alert(
+        "Error",
+        "An error occurred during logout. Please try again."
+      );
+    }
+  };
+
+  if (isLoggedIn) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Logged In</Text>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{isLogin ? "Login" : "Sign Up"}</Text>
+
+      {!isLogin && (
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+        />
+      )}
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      {!isLogin && (
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      )}
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={isLogin ? handleLogin : handleSignup}
+      >
+        <Text style={styles.buttonText}>{isLogin ? "Login" : "Sign Up"}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={toggleForm}>
+        <Text style={styles.switchText}>
+          {isLogin
+            ? "Don't have an account? Sign Up"
+            : "Already have an account? Login"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f8f9fa",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    padding: 10,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 8,
+  },
+  button: {
+    width: "100%",
+    padding: 15,
+    backgroundColor: "#007bff",
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  switchText: {
+    color: "#007bff",
+    marginTop: 20,
+  },
+});
+
+export default Two;
